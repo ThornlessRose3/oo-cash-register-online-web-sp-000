e8890a8f6bb7c7d7164799c2b27cfe462d7b8963
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-01 16:29:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount, :cart, :previous_total
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+    @previous_total = 0
+  end
+  
+  def add_item(name, cost, quantity = 1)
+    cart = @cart
+    i = 1
+    while i >= quantity
+      cart = cart.push(name)
+      i = i + 1
+    end
+    @cart = cart
+    @previous_total = @total
+    subtotal = @previous_total + (cost * quantity)
+    @total = subtotal
+  end
+  
+  def apply_discount
+    disc = @discount
+    result =""
+    if disc > 0
+      disc = disc.to_i
+      disc = disc*0.01
+      @previous_total = @total
+      subtotal = @total
+      subtotal = subtotal - (subtotal * disc)
+      subtotal = subtotal.to_i
+      @total = subtotal
+      result = "After the discount, the total comes to $#{@total}."
+    else
+      result = "There is no discount to apply."
+    end
+    return result
+  end
+  
+  def items
+    @cart
+  end
+  
+  
+end
